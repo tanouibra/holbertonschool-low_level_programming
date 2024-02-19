4b0c8e80df5564e15ddf0946d210e9a596b63e5e
tanouibra <tanouibrahima8@gmail.com>
2024-02-19 05:24:06 -0800

    Hello puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -5,6 +5,6 @@
  * Return: Always 0.
  */
 int main(void){	
-puts("\"Programming is like building a multilingual puzzle"\n);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }	
