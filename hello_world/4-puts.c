9df43af3304732297da640f0c1257f5c1507df83
tanouibra <tanouibrahima8@gmail.com>
2024-02-19 04:18:50 -0800

    Hello puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 int main() {
-	strcpy(str, "programming is like building a multilingual puzzle");
-	puts (string);
+	puts("programming is like building a multilingual puzzle");
 	return 0;
 }	
