5d76f2ea6e2f3efbd1c95f7fadd864ef6e00f4ed
tanouibra <tanouibrahima8@gmail.com>
2024-02-19 06:42:49 -0800

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,5 +11,5 @@ printf("Size of float: %1d byte(s)\n", sizeof(float));
 printf("Size of double: %1d byte(s)\n", sizeof(double));
 print("size of long: %1d byte(s)\n", sizeof(long));
 printf("Size of long long: %1d byte(s)\n", sizeof(long long));
-return 0;
+return (0);
 }
