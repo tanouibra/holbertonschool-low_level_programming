1178afd379ea9a0223dca5676782357bdfa6e190
tanouibra <tanouibrahima8@gmail.com>
2024-02-19 06:51:57 -0800

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,7 +8,7 @@ int main(void)
 printf("Size of a char: %1d byte(s)\n", sizeof(char));
 printf("Size of an int: %1d byte(s)\n", sizeof(int));
 printf("Size of a float: %1d byte(s)\n", sizeof(float));
-print("size of a long: %1d byte(s)\n", sizeof(long));
-printf("Size of a long long: %1d byte(s)\n", sizeof(long long));
+print("size of a long: %1d byte(s)\n", sizeof(long int));
+printf("Size of a long long: %1d byte(s)\n", sizeof(long long int));
 return (0);
 }
