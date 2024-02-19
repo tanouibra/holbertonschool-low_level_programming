377cedd007ed9eab28d009b4bff3cf854a6f67ff
tanouibra <tanouibrahima8@gmail.com>
2024-02-19 04:31:25 -0800

    Hello puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-#include <string.h>
-int main() {
+/**
+ * main pints "Programming is like building a multilingual puzzle, folowed by a new line
+ * Return always 0.
+ */
+int main(void) {
 	puts("programming is like building a multilingual puzzle");
 	return 0;
 }	
