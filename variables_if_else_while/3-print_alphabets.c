628c52bed76c41ab71adda61c0485ac4c4a94edd
tanouibra <tanouibrahima8@gmail.com>
2024-02-20 06:35:04 -0800

    minuscule et majuscule

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -2,28 +2,26 @@
 #include <time.h>
 #include <stdlib.h>
 /**
- * main - main block
- * Write a program that prints the alphabet in lowercase, and then in uppercase, followed by a new line.
- *
- * Retrun: 0
+*main - main block
+* Write a program that prints the alphabet in lowercase, and then in uppercase, followed by a new line.
+* Retrun: 0
  */
 int main(void)
 {
 	char lowcase = "a";
 	char uppercase = "A";
+
 	while (lowcase <= "z")
-	{
+	{	
 		putchar(lowcase);
 		lowcase++;
 	}
 	while (uppercase <= "Z")
-	{
+	{	
 		putchar(uppercase);
-		uppercase++
+		uppercase++;
 	}
 	putchar("\n");
-	
+
 	return (0);
 }	
-
-
