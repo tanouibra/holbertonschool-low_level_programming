bbb951ed7394e2f21f1efb04b22b1331428a0aed
tanouibra <tanouibrahima8@gmail.com>
2024-02-20 02:03:14 -0800

    positive or negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 #include <time.h> 
 /**
 *main - main block
-*  This program will assign a random number to the variable n
-*  betty style doc for function main goes there
-*   Return: 0.
+* This program will assign a random number to the variable n
+* betty style doc for function main goes there
+* Return: 0.
 */
 int main(void)
 {
