091dd23b8dc58c308b5395670e2e64bc40ba207e
tanouibra <tanouibrahima8@gmail.com>
2024-02-20 04:52:48 -0800

    last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,16 +17,17 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (digit > 5)
+
 	{
 		printf("last digit of %d is %d and is greater 5\n", n, digit);
 	}
 	else if (digit == 0)
 	{
-		printf("last digit of %d is %d and is less than 6 and not 0\n", n, digit);
+		printf("last digit of %d is %d is 0\n", n, digit);
 	}
 	else (digit < 6)
 	{
-		printf("last digit of %d is %d and is 0\n", n, digit);
+		printf("last digit of %d is %d and is less than 6\n", n, digit);
 	}
 	return (0);
 }
