42a8fbc8290d7d261b2083e6aee2940afed0cae5
tanouibra <tanouibrahima8@gmail.com>
2024-02-20 00:01:32 -0800

    positive or negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -14,7 +13,7 @@ if (n > 0)
 printf("%d is positive\n", n);
 }
 else if (n < 0)
-{	
+{
 printf("%d is negative\n", n);
 }
 else
