238e888498cacdec0a4372935a758746ce30e572
tanouibra <tanouibrahima8@gmail.com>
2024-02-20 03:16:20 -0800

    last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/**
+ *main - main block
+ * This program will assign a random number to the variable n
+ * betty style doc for function main goes there
+ * Return: 0
+  */
 int main(void){
 	int n;
 
