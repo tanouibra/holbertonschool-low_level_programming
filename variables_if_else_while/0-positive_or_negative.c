c6931ac06cfa5c02d4fce96cbcb005de7ac6bc74
tanouibra <tanouibrahima8@gmail.com>
2024-02-20 00:03:22 -0800

    positive or negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+/* main - main block */
 /* betty style doc for function main goes there */
 int main(void)
 {
