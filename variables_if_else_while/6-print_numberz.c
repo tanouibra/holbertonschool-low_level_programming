ba8e25a9a7797bdcdd73cb8a32fa773d26ae97cd
tanouibra <tanouibrahima8@gmail.com>
2024-02-21 00:26:30 -0800

    numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - main block
+ * numbers again
+ * Return: 0 
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
