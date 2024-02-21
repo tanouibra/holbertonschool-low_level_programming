ecc5a4f53761bf3c8d8757c3fefdd6dc83f4d756
tanouibra <tanouibrahima8@gmail.com>
2024-02-21 00:03:08 -0800

    numero 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
