013feafadcc4a78534b2030b75410639a8292af8
tanouibra <tanouibrahima8@gmail.com>
2024-02-21 01:52:30 -0800

    base 16

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,15 +10,17 @@ int main(void)
 {	
 	int i;
 
-	for (i = 0; i < 10; i++) {
+	for (i = 0; i < 10; i++)
+       	{
 		putchar(i + '0');
 	}
 
-	for (i = 0; i < 6; i++) {
+	for (i = 0; i < 6; i++)
+       	{
 		putchar(i + 'a');
 	}
 
 	putchar('\n');
 
-	return 0;
+	return (0);
 }
