b666d237ef1535c5b6b392bd2774db237de02d36
tanouibra <tanouibrahima8@gmail.com>
2024-02-21 05:31:41 -0800

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -4,6 +4,14 @@
 #include <time.h>
 int main(void)
 {
-	printf("_putchar\n");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
 }
