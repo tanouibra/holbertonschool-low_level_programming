7cd1bfad6635f5f063179a8d0b4f45074a8f55e3
tanouibra <tanouibrahima8@gmail.com>
2024-02-21 01:48:27 -0800

    base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main block
+ * base 16
+ * Return: 0
+ */
+int main(void)
+{	
+	int i;
+
+	for (i = 0; i < 10; i++) {
+		putchar(i + '0');
+	}
+
+	for (i = 0; i < 6; i++) {
+		putchar(i + 'a');
+	}
+
+	putchar('\n');
+
+	return 0;
+}
