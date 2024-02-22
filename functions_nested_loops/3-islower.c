aac1afdcd1b4cefda8a73eaf8a406404e8a21971
tanouibra <tanouibrahima8@gmail.com>
2024-02-22 11:36:47 +0000

    islwer

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * int _islower - ckeck lowcase
+ * @c: The character to be checked
  *  Return: 0 for lowcase character and 0 for anything else
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-		return 1;
+		return (1);
 	else
-		return 0;
+		return 0();
 }
