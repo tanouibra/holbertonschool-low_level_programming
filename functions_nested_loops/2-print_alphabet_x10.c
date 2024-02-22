5452875bcf9fe13dc1795fb0fd3311457cfa7385
tanouibra <tanouibrahima8@gmail.com>
2024-02-22 11:10:40 +0000

    alphabet *10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,11 +5,11 @@
  * Return: void
  */
 void print_alphabet_x10(void)
-{	
+{
 	char letter;
 	int i = 0;
 
-	while (i++ <= 9 )
+	while (i++ <=  9)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 
