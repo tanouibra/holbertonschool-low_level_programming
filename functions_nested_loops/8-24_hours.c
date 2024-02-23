2cc86738e016b7e5d8b16d459deb57ed2fdbdf5e
tanouibra <tanouibrahima8@gmail.com>
2024-02-23 13:58:36 +0000

    Jack Bauer

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,8 +9,10 @@ void jack_bauer(void)
 	int hour;
 	int minute;
 
-	for (hour = 0; hour < 24; hour++) {
-		for (minute = 0; minute < 60; minute++) {
+	for (hour = 0; hour < 24; hour++)
+       	{
+		for (minute = 0; minute < 60; minute++)
+	       	{
 			/* Print the hour (tens and ones digits)*/
 			_putchar(hour / 10 + '0');
 			_putchar(hour % 10 + '0');
