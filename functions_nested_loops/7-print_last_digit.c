df842d339fc21f3b13a8e8b05761358872b55ec0
tanouibra <tanouibrahima8@gmail.com>
2024-02-23 13:25:07 +0000

    last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,6 +8,8 @@
 int print_last_digit(int n)
 {
 	int last_digit = n % 10;
-	_putchar('0' + last_digit);
+	if (last_digit < 0)
+		last_digit = last_digit * -1;
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
