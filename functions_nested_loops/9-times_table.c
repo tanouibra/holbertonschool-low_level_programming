130c6abe2220f68c07ba4eb0832cce8461197d17
tanouibra <tanouibrahima8@gmail.com>
2024-02-23 14:41:10 +0000

    table times

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,35 +6,26 @@
  */
 void times_table(void)
 {
-	int i, j, result;
+	int num, mult, prod;
 
-	/* Loop through each row*/
-	for (i = 0; i <= 9; i++)
+	for (num = 0; num <= 9; num++)
 	{
-		/* Loop through each column*/
-		for (j = 0; j <= 9; j++)
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
 		{
-			result = i * j; /* Calculate the multiplication result*/
-			/* Print the result*/
-			if (result < 10)
-			{
-				_putchar(result + '0'); /* Print the ones digit*/
-			}
-			else
-			{
-				_putchar(result / 10 + '0'); /* Print the tens digit*/
-				_putchar(result % 10 + '0'); /* Print the ones digit*/
-			}
-			/* Print a comma and space unless it's the last column*/
-			if (j < 9)
-			{
-				_putchar(',');
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
 				_putchar(' ');
-			}
 			else
-			{
-				_putchar('\n'); /* Newline at the end of each row*/
-			}
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
 		}
+		_putchar('\n');
 	}
 }
