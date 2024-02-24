67584bdf9c96c7e479a5f5642285c971225b41f5
tanouibra <tanouibrahima8@gmail.com>
2024-02-24 17:23:41 +0000

    addition

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Add two values 
+ * @a: The first number to be computed
+ * @b: The second number to be computed
+ * Return: Integer values
+ */
+int add(int a, int b)
+{
+
+	return (a + b);
+}
