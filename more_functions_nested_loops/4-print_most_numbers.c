7826834ec5e72356259b503cb461067d4137585c
tanouibra <tanouibrahima8@gmail.com>
2024-02-26 10:10:50 +0000

    I believe in numbers and signs

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,11 +2,12 @@
 
 /**
  * print_most_numbers - Prints the numers from 0 to 9
- * Return: void 
+ * Return: void
  */
 void print_most_numbers(void)
 {
 	int i;
+
 	for (i = 0; i <= 9; i++)
 	{
 		if (i != 2 && i != 4)
