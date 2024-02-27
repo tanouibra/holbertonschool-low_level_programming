8e243c6a8790847bc89415e9960445b190d6c697
tanouibra <tanouibrahima8@gmail.com>
2024-02-27 09:58:57 +0000

    triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -12,7 +12,7 @@ void print_triangle(int size)
 
 	if (size <= 0)
 		_putchar('\n');
-	for (i = 1; i < size; i++)
+	for (i = 1; i <= size; i++)
 	{
 		for (j = 1; j <= size - i; j++)
 			_putchar(' ');
