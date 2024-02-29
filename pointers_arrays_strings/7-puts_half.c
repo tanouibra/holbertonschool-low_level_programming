24d41d8f698829cb75cb5c6dce2c8f4929c69009
tanouibra <tanouibrahima8@gmail.com>
2024-02-29 10:47:09 +0000

    half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_hal - Prints half of string
+ * @str: string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len = _strlen(str);
+
+	if (len % 2 == 0)
+		len = len / 2;
+
+	else
+		len = (len + 1) / 2;
+
+	while (str[len] != '\0')
+	{
+		_putchar(str[len]);
+		len++;
+	}
+	_putchar('\n');
+}
