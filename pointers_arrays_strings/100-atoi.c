dd4106a3a4941794a8f826dac0b40949065da56a
tanouibra <tanouibrahima8@gmail.com>
2024-03-01 13:29:11 +0000

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -15,13 +15,10 @@ int _atoi(char *s)
        	{
 		if (*s == '-')
 			sign *= -1;
-
 		else if (*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
-
 		else if (num > 0)
 			break;
-
 	}
        	while (*s++);
 
