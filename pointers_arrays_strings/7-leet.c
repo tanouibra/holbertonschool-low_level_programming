6937037493565dba874238582ec6d188a6d50931
tanouibra <tanouibrahima8@gmail.com>
2024-03-03 14:19:53 +0000

    7ltt

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,19 +9,17 @@
 char *leet(char *s)
 {
 	int i;
+	int j;
+	char n1[] = "aAeEoOtTlL";
+	char n2[] = "4433007711";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == n1[j])
+				s[i] = n2[j];
+		}
 	}
 
 	return (s);
