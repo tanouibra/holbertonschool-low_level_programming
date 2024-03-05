b7ce6ce74851af482b7af231efbc8f277a06eaf0
tanouibra <tanouibrahima8@gmail.com>
2024-03-05 10:22:25 +0000

    factorial

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -3,7 +3,7 @@
 /**
  * factorial - Returns the factoriel of a givent number
  * @n: number
- * Retrun: -1 if n < 0; 1 if n == 0; or factoriel(n)
+ * Retrun: factorial number
  */
 int factorial(int n)
 {
