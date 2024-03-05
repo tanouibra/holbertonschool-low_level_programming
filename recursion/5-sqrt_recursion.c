eb5397a5d30329d1dc65bc81b4bf246706a298b3
tanouibra <tanouibrahima8@gmail.com>
2024-03-05 13:41:56 +0000

    racine care

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,18 +1,15 @@
-#include "main.h"
+int helper(int n, int i) {
+    if (i * i == n)
+        return i;
+    if (i * i > n)
+        return -1;
+    return helper(n, i + 1);
+}
 
-/**
- * _sqrt_recursion - Returns the natural square root of a number
- * @n: number
- * Return: sqrt number.
- */
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-		return (-1);
-	else if (n == 0)
-		return (0);
-	else if (n == 1)
-		return (1);
-	else
-		return (n * _sqrt_recursion(n - 1));
+int _sqrt_recursion(int n) {
+    if (n < 0)
+        return -1;
+    if (n == 0 || n == 1)
+        return n;
+    return helper(n, 1);
 }
