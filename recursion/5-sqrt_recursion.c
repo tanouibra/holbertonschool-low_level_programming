416129303f55e78b9e95bcde6b15c6305ab3fa5f
tanouibra <tanouibrahima8@gmail.com>
2024-03-05 13:32:38 +0000

    racine care

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -9,8 +9,10 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if (n == 0 || n == 1)
+	else if (n == 0)
 		return (0);
+	else if (n == 1)
+		return (1);
 	else
 		return (_sqrt_recursion(n));
 }
