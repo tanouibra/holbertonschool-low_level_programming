e0fc55411d67fe83da9b815109b7df1d04122a10
tanouibra <tanouibrahima8@gmail.com>
2024-03-05 10:19:34 +0000

    factorial

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -7,10 +7,10 @@
  */
 int factorial(int n)
 {
-	if (n < 0)
-		return (-1);
-	else if (n == 0)
+	if (n == 0 || n == 1)
 		return (1);
+	else if (n > 1)
+		return (n * factorial(n - 1));
 	else
-		return (factorial(n));
+		return (-1);
 }
