c04f890d58f7a70a973d1e00372e42ea119d35cf
tanouibra <tanouibrahima8@gmail.com>
2024-03-13 09:57:10 +0000

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,25 +1,27 @@
-#include "main.h"
 #include <stdlib.h>
 
 /**
  * _calloc - Allocate memory for array of nmemb elements of size bytes
  * @nmemb: Number of elemnts
  * @size: Size in bytes of elements
+ *
  * Return: Pointer to new memory, NULL if it fails
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *pot;
-	unsigned i, art;
+	char *ar;
+	unsigned int ar_size, i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	art = nmemb * size;
-	pot = malloc(art);
-	while ( i < art)
+	ar_size = nmemb * size;
+	ar = malloc(ar_size);
+	if (ar == NULL)
+		return (NULL);
+	while (i < ar_size)
 	{
-		pot[i] = 0;
+		ar[i] = 0;
 		i++;
 	}
-	return (pot);
+	return (ar);
 }
